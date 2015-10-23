3a0f9f2f5b73cb22275027573c46b932d6d7a8a3
北川善彬 <ace1235813@gmail.com>
2015-10-23 11:50:08 +0900

    [Add] second commit; Nothing Error

--- src/train_feedforward_simple.py
+++ src/train_feedforward_simple.py
@@ -7,9 +7,11 @@ def create_vocab():
     vocab = dict()
     for f in [train_file, test_file]:
         for line in open(f):
-            for char in ''.join(line.split()):
+            for char in ''.join(line.strip().split()):
                 if char not in vocab:
                     vocab[char] = len(vocab)
+    vocab['<s>'] = len(vocab)
+    vocab['</s>'] = len(vocab)
     return vocab
 
 def init_model(vocab_size):
@@ -27,7 +29,7 @@ def make_label(sent):
     labels = list()
     for char in sent:
         if not char == ' ':
-            labels.append(0)
+            labels.append(1)
     return labels
 
 def train(char2id, model, optimizer):
@@ -36,11 +38,10 @@ def train(char2id, model, optimizer):
         batch_count = 0
         accum_loss = 0
         for line in open(train_file):
-            x = ''.join(line.split())
-            t = make_label(line)
+            x = ''.join(line.strip().split())
+            t = make_label(line.strip())
             for target in range(len(x)):
                 label = t[target]
-                print(x, target, label)
                 pred, loss = forward_one(x, target, label)
                 accum_loss += loss
                 batch_count += 1
@@ -58,11 +59,37 @@ def train(char2id, model, optimizer):
             accum_loss = 0
             batch_count = 0
 
+def test(char2id, model, optimizer):
+    sum_accuracy = 0
+    sum_loss = 0
+    
+    batch_count = 0
+    accum_loss = 0
+    labels = list()
+    for line in open(train_file):
+        x = ''.join(line.strip().split())
+        t = make_label(line.strip())
+        for target in range(len(x)):
+            label = t[target]
+            labels.append(label)
+            loss, acc = forward_one(x, target, label)
+        print (x)
+        print (labels)
+        print (t)
+    #sum_loss += float(loss.data) * len(y)
+    #sum_accuracy += float(acc.data) * len(y)
+
+#print('test  mean loss={}, accuracy={}'.format(
+#    sum_loss / N_test, sum_accuracy / N_test))
+
 def forward_one(x, target, label):
     # make input window vector
-    #distance = 2 // window
     distance = window // 2
     char_vecs = list()
+    x = list(x)
+    for i in range(distance):
+        x.append('</s>')
+        x.insert(0,'<s>')
     for i in range(-distance, distance + 1):
         char = x[target + i]
         char_id = char2id[char]
@@ -95,4 +122,5 @@ if __name__ == '__main__':
     char2id = create_vocab()
     model, opt = init_model(len(char2id))
     train(char2id, model, opt)
-
+    test(char2id, model, opt)
+    
